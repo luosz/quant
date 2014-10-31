5bda3b74c36a4b8ecf68e5360cdcbc72c3a4b391
Joe <luos@tcd.ie>
2014-10-31 23:52:57 +0000

    remove a file

--- sma_crossover_plot.py
+++ sma_crossover_plot.py
@@ -1,7 +1,43 @@
-import sma_crossover
+from pyalgotrade import strategy
+from pyalgotrade.technical import ma
+from pyalgotrade.technical import cross
 from pyalgotrade import plotter
 from pyalgotrade.tools import yahoofinance
 from pyalgotrade.stratanalyzer import sharpe
+
+class SMACrossOver(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, smaPeriod):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        self.__position = None
+        # We'll use adjusted close values instead of regular close values.
+        self.setUseAdjustedValues(True)
+        self.__prices = feed[instrument].getPriceDataSeries()
+        self.__sma = ma.SMA(self.__prices, smaPeriod)
+
+    def getSMA(self):
+        return self.__sma
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onExitOk(self, position):
+        self.__position = None
+
+    def onExitCanceled(self, position):
+        # If the exit was canceled, re-submit it.
+        self.__position.exitMarket()
+
+    def onBars(self, bars):
+        # If a position was not opened, check if we should enter a long position.
+        if self.__position is None:
+            if cross.cross_above(self.__prices, self.__sma) > 0:
+                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                # Enter a buy market order. The order is good till canceled.
+                self.__position = self.enterLong(self.__instrument, shares, True)
+        # Check if we have to exit the position.
+        elif not self.__position.exitActive() and cross.cross_below(self.__prices, self.__sma) > 0:
+            self.__position.exitMarket()
 
 def main(plot):
     instrument = "aapl"
@@ -10,7 +46,7 @@ def main(plot):
     # Download the bars.
     feed = yahoofinance.build_feed([instrument], 2011, 2012, ".")
 
-    strat = sma_crossover.SMACrossOver(feed, instrument, smaPeriod)
+    strat = SMACrossOver(feed, instrument, smaPeriod)
     sharpeRatioAnalyzer = sharpe.SharpeRatio()
     strat.attachAnalyzer(sharpeRatioAnalyzer)
 
